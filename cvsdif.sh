4ec26c29e47f73bfa45129643660a6b58eb62454
Ryan Davis <ryand-ruby@zenspider.com>
1999-08-18 13:00:37 -0800

    Stripped deadweight.
    [git-p4: depot-paths = "//usr/ryand/Bin/": change = 134]

--- cvsdif.sh
+++ cvsdif.sh
@@ -1,19 +1,8 @@
 #! /bin/sh
 
-if [ -d CVS.adm ]; then
-  cvsTool=l2cvs
-  for i in $*
-  do
-    j=`$cvsTool status $i | grep "RCS:" | cut -f2`
-    $cvsTool diff -r$j $i
-  done
-else
-  cvsTool=cvs
-  for i in $*
-  do
-#    j=`$cvsTool status $i | grep RCS | cut -f2`
-#    $cvsTool diff -r$j $i
-    $cvsTool diff $i
-  done
-fi
+cvsTool=cvs
+for i in $*
+do
+  $cvsTool diff $i
+done
 
