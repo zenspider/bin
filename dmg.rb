124cdc03246c5f2332e13fb61e737f4324e073d3
Ryan Davis <ryand-ruby@zenspider.com>
2004-11-22 21:05:59 -0800

    Added
    [git-p4: depot-paths = "//usr/ryand/Bin/": change = 1783]

---
+++ dmg.rb
@@ -0,0 +1,19 @@
+#!/usr/local/bin/ruby -w
+
+# usage error
+if ARGV.length < 1 or ARGV.length > 2 then
+  $stderr.puts( "Usage: #{File.basename( $0 )} directory [cd name]" )
+  exit( 1 )
+end
+
+# get variables
+dir = ARGV.shift.sub( /\/$/, "" )
+volname = if ARGV.length == 0 then
+            File.basename( File.expand_path( dir ) )
+          else
+            ARGV.shift
+          end
+output = "#{dir}.dmg"
+
+# dmg the folder
+puts "hdiutil create -fs HFS+ -srcfolder \"#{dir}\" -volname \"#{volname}\" \"#{output}\""
