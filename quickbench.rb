a5a43a852097f4b1f159a310ed68c7fdf47b4a9c
Ryan Davis <ryand-ruby@zenspider.com>
2005-07-12 19:01:21 -0800

    Made many of my find scripts svn resiliant. Much better use of $@ so multi arg usage will work better. Fixed some emacs stuffs. Improved quickbench a tad.
    [git-p4: depot-paths = "//usr/ryand/Bin/": change = 2101]

--- quickbench.rb
+++ quickbench.rb
@@ -12,16 +12,15 @@ end
 count = ARGV.shift.to_i || usage
 usage unless ARGV.empty?
 
+print <<'EOM'
 require 'benchmark'
 
-print <<EOM
-require 'benchmark'
+max = (ARGV.shift || 1_000_000).to_i
 
-MAX=1_000_000
-
+puts "# of iterations = #{max}"
 Benchmark::bm(20) do |x|
   x.report("null_time") { 
-    for i in 0..MAX do
+    for i in 0..max do
       # do nothing
     end
   }
@@ -31,7 +30,7 @@ EOM
 count.times do |n|
   print <<"EOM"
   x.report("benchmark-#{n+1}") { 
-    for i in 0..MAX do
+    for i in 0..max do
       # insert code here
     end
   }
