965b13d817fe6a6a5bc3be13992b80bdce964158
Ryan Davis <ryand-ruby@zenspider.com>
2009-03-04 14:46:46 -0800

    Added dock app clearing [git-p4: depot-paths = "//usr/ryand/Bin/": change = 4674]

--- osx_fix_dock.sh
+++ osx_fix_dock.sh
@@ -1,5 +1,6 @@
 defaults write com.apple.dock pinning -string start
 defaults write com.apple.dock no-glass -boolean YES
+defaults delete com.apple.dock persistent-apps
 killall Dock
 defaults write com.apple.Finder QuitMenuItem -boolean YES
 killall Finder
