3c4b3b80bb564e258110783c6f35095ceddd7fcf
Ryan Davis <ryand-ruby@zenspider.com>
2010-11-11 21:01:38 -0800

    Added extra tweaks for finder, loginwindow, and iphoto [git-p4: depot-paths = "//usr/ryand/Bin/": change = 5971]

--- osx_fix_dock.sh
+++ osx_fix_dock.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
+
 defaults write com.apple.dock pinning -string start
 defaults write com.apple.dock no-glass -boolean YES
 defaults delete com.apple.dock persistent-apps
 killall Dock
 defaults write com.apple.Finder QuitMenuItem -boolean YES
+defaults write com.apple.finder QLEnableXRayFolders -boolean YES
 killall Finder
+defaults write com.apple.iphoto MapScrollWheel -bool YES
 
+sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Property of Ryan Davis - 206.999.9936"
