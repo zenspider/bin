0890a6b76e7a7233ad73774e09b9a7aa4383c8bd
Ryan Davis <ryand-ruby@zenspider.com>
2011-06-22 15:53:44 -0800

    extra default tweaks [git-p4: depot-paths = "//usr/ryand/Bin/": change = 6348]

--- osx_fix_dock.sh
+++ osx_fix_dock.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-defaults write -g NSInitialToolTipDelay -int 10000
+# oh god do I hate tooltips
+defaults write -g NSInitialToolTipDelay -int 30000
 
 defaults write com.apple.dock pinning -string start
+defaults write com.apple.dock autohide -boolean YES
 defaults write com.apple.dock no-glass -boolean YES
 defaults delete com.apple.dock persistent-apps
+defaults write com.apple.dock use-new-list-stack -boolean yes
 killall Dock
+
 defaults write com.apple.Finder QuitMenuItem -boolean YES
 defaults write com.apple.finder QLEnableXRayFolders -boolean YES
 killall Finder
-defaults write com.apple.iphoto MapScrollWheel -bool YES
 
-sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Property of Ryan Davis - 206.999.9936"
+defaults write com.apple.iphoto MapScrollWheel -bool YES
+killall iPhoto
+
+defaults write com.apple.iTunes show-store-link-arrows -bool YES
+defaults write com.apple.iTunes show-store-arrow-links -bool YES
+killall iTunes
+
+sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText "Property of Ryan Davis - ATTi - 206.999.9936"
+
