69250fa8630ae3c65ca8db92edb4cd104c1c2d64
Ryan Davis <ryand-ruby@zenspider.com>
2019-04-16 11:54:14 -0800

    Convert into a two-pass system that avoids having type directories with singlo files in them. [git-p4: depot-paths = "//usr/ryand/Bin/": change = 11970]

--- clean_by_type.rb
+++ clean_by_type.rb
@@ -1,16 +1,57 @@
 #!/usr/bin/env ruby -w
 
-require 'fileutils'
+require "pp"
+require "fileutils"
 
 system 'find . -name .DS_Store -exec rm {} \;'
 system 'find . -maxdepth 1 -empty -type d -exec rmdir {} \;'
 
-Dir['*'].each do |f|
-  next if test ?d, f
+files = Hash.new { |h, k| h[k] = [] }
 
-  ext = File.extname(f)
-  next if ext.empty?
-  ext = ext[1..-1]
-  Dir.mkdir(ext) unless test ?d, ext
-  FileUtils.mv f, ext unless test ?f, File.join(ext, f)
+Dir["*"].each do |f|
+  if File.file? f then
+    ext = File.extname(f).delete_prefix "."
+
+    files[ext] << f
+  elsif File.directory? f then
+    d = f
+
+    unless d =~ /^[a-z]+$/ then
+      warn "# skipping #{d}"
+      next
+    end
+
+    underfiles = Dir["#{d}/*.#{d}"]
+
+    files[d].concat underfiles
+  else
+    warn "# unprocessed: #{f}"
+  end
+end
+
+fileutils = FileUtils::Verbose
+
+files.each do |ext, paths|
+  # only one entry in subdir = unpack
+  # only one entry at top    = leave alone
+  # otherwise                = make directory, move all files into subdirectory
+
+  if paths.size == 1 then
+    path = paths.first
+    if path =~ %r%/% then
+      warn "# unpacking #{path}"
+      fileutils.mv path, "."
+      fileutils.rmdir ext
+    end
+  else
+    move = paths.grep_v %r%/%
+
+    unless move.empty? then
+      warn "# organizing #{ext}"
+
+      fileutils.mkdir ext unless File.directory? ext
+
+      fileutils.mv move, ext
+    end
+  end
 end
