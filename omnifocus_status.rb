ce20e8f4db4e6dfddc754ae539dcb23c7c145051
Ryan Davis <ryand-ruby@zenspider.com>
2020-02-14 16:51:30 -0800

    cleaned up omnifocus_status.rb by folding it in with of_common.rb [git-p4: depot-paths = "//usr/ryand/Bin/": change = 12568]

--- omnifocus_status.rb
+++ omnifocus_status.rb
@@ -1,27 +1,7 @@
 #!/usr/bin/env ruby -w
 
-db = File.expand_path ["~/Library/Containers",
-                       "com.omnigroup.OmniFocus3/Data/Library",
-                       "Application Support",
-                       "OmniFocus/OmniFocus Caches",
-                       "OmniFocusDatabase"].join "/"
+require_relative "./of_common.rb"
 
-unless File.file? db
-  db = File.expand_path ["~/Library/Containers",
-                         "com.omnigroup.OmniFocus2/Data/Library",
-                         "Caches", "com.omnigroup.OmniFocus2",
-                         "OmniFocusDatabase2"].join "/"
-end
-
-soon = Time.now + 86400 - Time.new(2001, 1, 1)
-
-cmd = ["sqlite3 %p 'select COUNT(*) from task",
-       "left join projectinfo p on task.containingprojectinfo=p.pk",
-       "where (folderEffectiveActive is null or folderEffectiveActive = 1)",
-       'and (status is null or status = "active")',
-       "and dateCompleted is null",
-       "and effectiveDateDue < %d'"].join(" ") % [db, soon]
-
-count = `#{cmd}`.to_i
-
-puts count
+of = Omnifocus.new
+results = of.results :count_soon
+p results.flatten.first.to_i
