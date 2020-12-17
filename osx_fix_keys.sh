0a97ee8c8c3d02c0ec16ce1ea858d3abbf728840
Ryan Davis <ryand-ruby@zenspider.com>
2020-12-17 13:18:32 -0800

    Added [git-p4: depot-paths = "//usr/ryand/Bin/": change = 12917]

---
+++ osx_fix_keys.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby -w
+
+{
+  "com.apple.iWork.Keynote" => {
+    "Collapse All"             => "@~0",
+    "Expand All"               => "@~9",
+    "Fit in Window"            => "@~=>",
+    "Light Table"              => "@~l",
+    "Navigator"                => "@~n",
+    "Outline"                  => "@~u",
+    "Play Slideshow"           => "@p",
+    "Reapply Master to Slide"  => "@^m",
+    "Reapply Master to Slides" => "@^m",
+    "Rehearse Slideshow"       => "@r",
+  }
+}.each do |domain, bindings|
+  bindings.each do |menu, binding|
+    system "defaults write %s NSUserKeyEquivalents -dict-add %p -string %p" \
+      % [domain, menu, binding]
+  end
+end
