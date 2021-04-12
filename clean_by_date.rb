97a795cae6b1c927ed261258efb7b18e9ae12eff
Ryan Davis <ryand-ruby@zenspider.com>
2021-04-12 13:32:00 -0800

    Add -t flag to do dated dirs by type as well [git-p4: depot-paths = "//usr/ryand/Bin/": change = 13064]

--- clean_by_date.rb
+++ clean_by_date.rb
@@ -1,4 +1,6 @@
-#!/usr/bin/env ruby -w
+#!/usr/bin/env ruby -ws
+
+$t ||= false
 
 require 'fileutils'
 
@@ -8,7 +10,7 @@ format = "%Y-%m"
 now = Time.now.strftime(format)
 
 Dir['*'].each do |f|
-  next if f =~ /^\d\d\d\d-\d\d|^@/
+  next if f =~ /^\d\d\d\d-\d\d$|^@/
   dir = File.mtime(f).strftime(format)
   p :skip => [f, dir] if dir == now
   next if dir == now
@@ -18,5 +20,15 @@ Dir['*'].each do |f|
     FileUtils.mv f, dir
   else
     puts "file #{f} exists in #{dir}"
+  end
+end
+
+if $t then
+  Dir['*'].each do |f|
+    next unless f =~ /^\d\d\d\d-\d\d$/
+
+    Dir.chdir f do
+      system "clean_by_type.rb"
+    end
   end
 end
