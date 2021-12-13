1536339c5a564920c6803679fe66b0e436a56fb2
Ryan Davis <ryand-ruby@zenspider.com>
2021-12-13 01:39:08 -0800

    Added [git-p4: depot-paths = "//usr/ryand/Bin/": change = 13283]

---
+++ osx_homebrew_flags.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/env ruby
+
+$: << "/opt/homebrew/Library/Homebrew"
+
+class Object
+  def present?
+    respond_to?(:empty?) ? !empty? : !!self
+  end
+end
+
+module Homebrew
+  module EnvConfig
+    def self.const_missing name
+      :NAH!
+    end
+
+    def self.sig
+      :NAH!
+    end
+  end
+end
+
+module T
+  module Sig
+  end
+end
+
+require "env_config"
+
+current = ENV.keys.grep(/HOMEBREW/)
+exclude = DATA.read.split
+
+man = `man brew`
+  .gsub(/\x08./, "") # delete \b<char> from bold manpage output
+  .scan(/HOMEBREW_\w+/)
+  .sort
+  .uniq
+
+envs = Homebrew::EnvConfig::ENVS.keys.map(&:to_s)
+
+(envs + man - exclude).uniq.each do |name|
+  next unless name =~ /BREW_NO_/ and not current.include? name
+
+  puts "export #{name}=1"
+end
+
+(current - envs - man).each do |name|
+  puts "Remove? #{name}"
+end
+
+__END__
+HOMEBREW_NO_BOOTSNAP
+HOMEBREW_NO_CLEANUP_FORMULAE
+HOMEBREW_NO_ENV_HINTS
